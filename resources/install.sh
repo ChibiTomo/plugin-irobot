7a36f9365647a5f7c03f9648d0e0c27bcf1f11c0
ChibiTomo <thomias.yannis@gmail.com>
2021-01-27 23:43:58 +0100

    Redirect standard error

--- resources/install.sh
+++ resources/install.sh
@@ -7,15 +7,15 @@ echo 0 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation des dépendances             *"
 echo "********************************************************"
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash &2>1
 echo 50 > ${PROGRESS_FILE}
 if [[ $NVM_DIR = "" ]]; then
     export NVM_DIR="$HOME/.nvm"
 fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install node
+nvm install node &2>1
 echo 75 > ${PROGRESS_FILE}
-npm install -g dorita980
+npm install -g dorita980 &2>1
 echo 100 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation terminée                    *"
